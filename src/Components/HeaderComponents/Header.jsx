b5ae5401111bb30f8f68a06ef5166265f1ed1cd7
Tayokanch <tayoquadri11@gmail.com>
2023-11-29 00:07:26 +0000

    added the form component

---
+++ src/Components/HeaderComponents/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Header.css'
+
+function Header() {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>Sell Ticket</li>
+      <li>Buy Ticket</li>
+      <li>About Us</li>
+    </ul>
+  );
+}
+
+export default Header;
---
+++ src/Components/MainComponents/Advert.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Advert.css";
+
+function Advert() {
+  return (
+    <section className="main-advert">
+      <p>
+        Embark on a journey of affordability with our unique platform that
+        brings together <span>Train Ticket </span>sellers from every corner of
+        the UK
+      </p>
+    </section>
+  );
+}
+
+export default Advert;
