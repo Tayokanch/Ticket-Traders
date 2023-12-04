d65d313fb02294fe7f18a8cc55c887c557716375
Tayokanch <tayoquadri11@gmail.com>
2023-12-04 20:51:18 +0000

    added media queries

--- src/Components/HeaderComponents/Header.jsx
+++ src/Components/HeaderComponents/Header.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
-  const navigate = useNavigate();
+  const [toggleIcon, setToggleIcon] = useState(false);
 
+  const handleclick = () => {
+    setToggleIcon(!toggleIcon);
+  };
+
+  const navigate = useNavigate();
   return (
-    <ul>
-      <li onClick={() => navigate("/")}>Home</li>
-      <li onClick={() => navigate("/Alltickets")}>Avialable Tickets</li>
-      <li onClick={() => navigate("/Purchase")}>Search Tickets</li>
-      <li>About Us</li>
-    </ul>
+    <>
+      <section className="icon-box">
+        <FontAwesomeIcon icon={faBars} className="icon" onClick={handleclick} />
+      </section>
+      <ul className={`${toggleIcon ? "active" : ""}`}>
+        <li onClick={() => navigate("/")}>Home</li>
+        <li onClick={() => navigate("/Alltickets")}>Avialable Tickets</li>
+        <li onClick={() => navigate("/Purchase")}>Search Tickets</li>
+        <li>About Us</li>
+      </ul>
+    </>
   );
 }
 
